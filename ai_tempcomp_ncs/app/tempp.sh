3a7898600cd45a187647c0d2968a9573b2480163
vitowen9580 <vito9580work@gmail.com>
2025-01-07 21:39:02 +0800

    git rm -r --cached

--- ai_tempcomp_ncs/app/tempp.sh
+++
@@ -1,2 +0,0 @@
-image_base64=$(<image_base64.txt)
-sudo influx -username 'root' -password 'eii123' -host localhost -database 'your_database' -execute "INSERT image_data image_base64=\"$image_base64\""
--- ai_tempcomp_ncs/run_container.sh
+++
@@ -1,2 +0,0 @@
-sudo docker exec -it --privileged  --user root ai_tempcomp_ncs /bin/bash
-
